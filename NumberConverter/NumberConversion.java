9d42585f507e6c9fea925c5000e420e28e90523f
thongvu <tdv225@lehigh.edu>
2023-06-06 03:51:48 -0600

    Update DecToBinary

--- NumberConverter/NumberConversion.java
+++ NumberConverter/NumberConversion.java
@@ -8,10 +8,11 @@ public class NumberConversion {
         String type = "";
 
         do {
-            System.out.println("Enter a type of value to be generated or quit to exit the program (hex, binary, decimal, quit): ");
+            System.out.println(
+                    "Enter a type of value to be generated or quit to exit the program (hex, binary, decimal, quit): ");
             type = input.nextLine().toLowerCase();
 
-            int randomNum = (int)(Math.random()*100) + 1; //random number between 1 and 10000
+            int randomNum = (int) (Math.random() * 100) + 1; // random number between 1 and 10000
             String hexVal, hexGuess, resultHex, binaryVal, binaryGuess, resultBinary;
             int decimalGuess, resultDecimal;
 
@@ -32,16 +33,17 @@ public class NumberConversion {
                     resultDecimal = convertHexToDec(hexVal);
                     resultBinary = convertHexToBinary(hexVal);
 
-                    if(decimalGuess == resultDecimal) {
+                    if (decimalGuess == resultDecimal) {
                         System.out.println("Your guess for the decimal value was correct!");
                     }
-                    if(resultBinary.equals(binaryGuess) || stripLeadZeros(resultBinary).equals(binaryGuess)) {
+                    if (resultBinary.equals(binaryGuess) || stripLeadZeros(resultBinary).equals(binaryGuess)) {
                         System.out.println("Your guess for the binary value was correct!");
                     }
 
                     System.out.println("-------------------------------------------------------------------------");
                     System.out.println("The decimal equivalent of " + hexVal + " is " + resultDecimal);
-                    System.out.println("The binary equivalent of " + hexVal + " is " + resultBinary + " or " + stripLeadZeros(resultBinary));
+                    System.out.println("The binary equivalent of " + hexVal + " is " + resultBinary + " or "
+                            + stripLeadZeros(resultBinary));
                     System.out.println("-------------------------------------------------------------------------");
                     break;
                 case "binary":
@@ -60,10 +62,10 @@ public class NumberConversion {
                     resultDecimal = convertBinaryToDec(binaryVal);
                     resultHex = convertBinaryToHex(binaryVal);
 
-                    if(decimalGuess == resultDecimal) {
+                    if (decimalGuess == resultDecimal) {
                         System.out.println("Your guess for the decimal value was correct!");
                     }
-                    if(resultHex.equals(hexGuess)) {
+                    if (resultHex.equals(hexGuess)) {
                         System.out.println("Your guess for the hex value was correct!");
                     }
 
@@ -87,37 +89,39 @@ public class NumberConversion {
                     resultBinary = convertDecToBinary(randomNum);
                     resultHex = convertDecToHex(randomNum);
 
-                    if(resultBinary.equals(binaryGuess) || stripLeadZeros(resultBinary).equals(binaryGuess)) {
+                    if (resultBinary.equals(binaryGuess) || stripLeadZeros(resultBinary).equals(binaryGuess)) {
                         System.out.println("Your guess for the binary value was correct!");
                     }
-                    if(resultHex.equals(hexGuess)) {
+                    if (resultHex.equals(hexGuess)) {
                         System.out.println("Your guess for the hex value was correct!");
                     }
 
                     System.out.println("-------------------------------------------------------------------------");
-                    System.out.println("The binary equivalent of " + randomNum + " is " + resultBinary + " or " + stripLeadZeros(resultBinary));
+                    System.out.println("The binary equivalent of " + randomNum + " is " + resultBinary + " or "
+                            + stripLeadZeros(resultBinary));
                     System.out.println("The hex equivalent of " + randomNum + " is " + resultHex);
                     System.out.println("-------------------------------------------------------------------------");
                     break;
-                case "quit": break;
+                case "quit":
+                    break;
                 default:
                     System.out.println("Wrong Type, Please Try Again.");
                     break;
             }
             input.nextLine();
-        } while(!type.equals("quit"));
+        } while (!type.equals("quit"));
         input.close();
 
     }
 
     public static int convertHexToDec(String input) {
         int result = 0;
-        for(int i = 0; i < input.length(); i++) {
-            int char0 = (int) input.charAt( (input.length()-1) - i); // moves through the input backwards
-            if(char0 <= 102 && char0 >= 97) { // if the input is a character between 'a' to 'f'
-                char0 -= 87; //convert to decimal value
-            } else if(char0 <= 57 && char0 >= 48) { // if the input is a character between '0' to '9'
-                char0 -= 48; //convert to decimal value
+        for (int i = 0; i < input.length(); i++) {
+            int char0 = (int) input.charAt((input.length() - 1) - i); // moves through the input backwards
+            if (char0 <= 102 && char0 >= 97) { // if the input is a character between 'a' to 'f'
+                char0 -= 87; // convert to decimal value
+            } else if (char0 <= 57 && char0 >= 48) { // if the input is a character between '0' to '9'
+                char0 -= 48; // convert to decimal value
             }
             result += ((int) Math.pow(16.0, (double) i)) * ((int) char0); // 16^i * char0
         }
@@ -126,14 +130,14 @@ public class NumberConversion {
 
     public static String convertHexToBinary(String input) {
         String resultStr = "";
-        for(int i = 0; i < input.length(); i++) {
-            int char0 = (int) input.charAt( (input.length()-1) - i); // moves through the input backwards
-            if(char0 <= 102 && char0 >= 97) { // if the input is a character between 'a' to 'f'
-                char0 -= 87; //convert to decimal value
-            } else if(char0 <= 57 && char0 >= 48) { // if the input is a character between '0' to '9'
-                char0 -= 48; //convert to decimal value
+        for (int i = 0; i < input.length(); i++) {
+            int char0 = (int) input.charAt((input.length() - 1) - i); // moves through the input backwards
+            if (char0 <= 102 && char0 >= 97) { // if the input is a character between 'a' to 'f'
+                char0 -= 87; // convert to decimal value
+            } else if (char0 <= 57 && char0 >= 48) { // if the input is a character between '0' to '9'
+                char0 -= 48; // convert to decimal value
             }
-            for(int j = 0; j < 4; j++) {
+            for (int j = 0; j < 4; j++) {
                 resultStr = (char0 % 2) + resultStr;
                 char0 /= 2;
             }
@@ -143,9 +147,9 @@ public class NumberConversion {
 
     public static int convertBinaryToDec(String input) {
         int result = 0;
-        for(int i = 0; i < input.length(); i++) {
-            int char0 = (int) input.charAt( (input.length()-1) - i); // moves through the input backwards
-            if(char0 < 48 || char0 > 49) {
+        for (int i = 0; i < input.length(); i++) {
+            int char0 = (int) input.charAt((input.length() - 1) - i); // moves through the input backwards
+            if (char0 < 48 || char0 > 49) {
                 System.out.println("Invalid binary string.");
                 return -1;
             }
@@ -158,30 +162,32 @@ public class NumberConversion {
     public static String convertBinaryToHex(String input) {
         int result = 0;
         String resultStr = "";
-        for(int i = 0; i < input.length(); i++) {
-            if(i%4 == 0 && i > 0) {
+        for (int i = 0; i < input.length(); i++) {
+            if (i % 4 == 0 && i > 0) {
                 char c;
-                if(result <= 15 && result >= 10) { // if the input is a character between 'a' to 'f'
-                    c = (char)(result + 55); //convert to appropriate Hex Value
+                if (result <= 15 && result >= 10) { // if the input is a character between 'a' to 'f'
+                    c = (char) (result + 55); // convert to appropriate Hex Value
                     resultStr = c + resultStr;
-                } else resultStr = "" + result + resultStr;
+                } else
+                    resultStr = "" + result + resultStr;
                 result = 0;
             }
-            int char0 = (int) input.charAt( (input.length()-1) - i); // moves through the input backwards
-            if(char0 < 48 || char0 > 49) {
+            int char0 = (int) input.charAt((input.length() - 1) - i); // moves through the input backwards
+            if (char0 < 48 || char0 > 49) {
                 System.out.println("Invalid binary string.");
                 result = -1;
                 break;
             }
             char0 -= 48;
-            result += ((int) Math.pow(2.0, (double) (i%4))) * ((int) char0); // 16^i * char0
+            result += ((int) Math.pow(2.0, (double) (i % 4))) * ((int) char0); // 16^i * char0
         }
-        if(result != 0) {
+        if (result != 0) {
             char c;
-            if(result <= 15 && result >= 10) { // if the input is a character between 'a' to 'f'
-                c = (char)(result + 55); //convert to appropriate Hex Value
+            if (result <= 15 && result >= 10) { // if the input is a character between 'a' to 'f'
+                c = (char) (result + 55); // convert to appropriate Hex Value
                 resultStr = c + resultStr;
-            } else resultStr = "" + result + resultStr;
+            } else
+                resultStr = "" + result + resultStr;
         }
 
         return resultStr;
@@ -189,12 +195,13 @@ public class NumberConversion {
 
     public static String convertDecToHex(int input) {
         String resultStr = "";
-        while(input > 0) {
+        while (input > 0) {
             int c = input % 16;
-            if(c >= 10) { // if the input is a character between 'a' to 'f'
-                resultStr = (char)(c+87) + resultStr; //convert to decimal value
+            if (c >= 10) { // if the input is a character between 'a' to 'f'
+                resultStr = (char) (c + 87) + resultStr; // convert to decimal value
             } else { // if the input is a character between '0' to '9'
-            resultStr = (char)(c+48) + resultStr;; //convert to decimal value
+                resultStr = (char) (c + 48) + resultStr;
+                ; // convert to decimal value
             }
             input /= 16;
         }
@@ -202,14 +209,25 @@ public class NumberConversion {
     }
 
     public static String convertDecToBinary(int input) {
-        String resultStr = convertDecToHex(input);
-        return convertHexToBinary(resultStr);
+        String resultStr = "";
+        for (int i = 0; i < 8; i++) {
+            int x = (input & 0xF0000000);
+            input <<= 4;
+            if(x == 0) continue;
+            else {
+                for(int j = 0; j < 4; j++) {
+                    resultStr += (x & 0x80000000) >>> 31;
+                    x <<= 1;
+                }
+            }
+        }
+        return resultStr;
     }
 
     public static String stripLeadZeros(String input) {
         int i;
-        for(i = 0; i < input.length(); i++) {
-            if(input.charAt(i) != '0') {
+        for (i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != '0') {
                 break;
             }
         }
